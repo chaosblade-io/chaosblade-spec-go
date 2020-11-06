2c7ef8fc0a4b4529e554ea322473b39d18e877d4
MandssS <1003810340@qq.com>
2020-11-06 13:37:43 +0800

    flag add a parameter:default
    Signed-off-by: tiny-x <xf.yefei@gmail.com>

--- spec/model.go
+++ spec/model.go
@@ -101,6 +101,8 @@ type ExpFlagSpec interface {
 	FlagRequired() bool
 	// FlagRequiredWhenDestroyed returns true if the flag is necessary when destroying experiment
 	FlagRequiredWhenDestroyed() bool
+	// 	FlagDefault return the flag Defaule
+	FlagDefault() string
 }
 
 // ExpFlag defines the action flag
@@ -118,6 +120,9 @@ type ExpFlag struct {
 	Required bool `yaml:"required"`
 	// RequiredWhenDestroyed is true if the flag is necessary when destroying experiment
 	RequiredWhenDestroyed bool `yaml:"requiredWhenDestroyed"`
+
+	// default value
+	Default string `yaml:"default,omitempty"`
 }
 
 func (f *ExpFlag) FlagName() string {
@@ -138,6 +143,10 @@ func (f *ExpFlag) FlagRequired() bool {
 
 func (f *ExpFlag) FlagRequiredWhenDestroyed() bool {
 	return f.RequiredWhenDestroyed
+}
+
+func (f *ExpFlag) FlagDefault() string {
+	return f.Default
 }
 
 // BaseExpModelCommandSpec defines the common struct of the implementation of ExpModelCommandSpec
