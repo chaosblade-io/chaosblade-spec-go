d97c6d482e75862aec8f0a0f471aa9a4652e376b
tiny-x <xf.yefei@gmail.com>
2022-04-01 11:29:07 +0800

    feature: add constans
    Signed-off-by: tiny-x <xf.yefei@gmail.com>

---
+++ spec/constants.go
@@ -0,0 +1,15 @@
+package spec
+
+const (
+	LocalChannel      = "local"
+	NSExecBin         = "nsexec"
+	ChaosOsBin        = "chaos_os"
+	Destroy           = "destroy"
+	Create            = "create"
+	True              = "true"
+	False             = "false"
+	BinPath           = "bin"
+	ExperimentId      = "experiment"
+	DefaultCGroupPath = "/sys/fs/cgroup/"
+	Uid               = "uid"
+)
